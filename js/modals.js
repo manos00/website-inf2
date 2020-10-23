ab67940b20c9c99af1d8d9bb1091a1322a927d36
manos00 <manos.wagner@gmail.com>
2020-10-23 16:35:22 +0200

    buttons

---
+++ js/modals.js
@@ -0,0 +1,167 @@
+/* modal 1 */
+
+/* define modal */
+var modal = document.getElementById("modalid");
+
+/* define button */
+var btn = document.getElementById("btnid");
+
+/* define <span> element that closes modal */
+var span = document.getElementsByClassName("close1")[0];
+
+/* function button opens modal */
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+/* on click of <span> (x) modal closes */
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+/* modal closes if clicked anywhere outside of modal */
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+/* modal 2 */
+
+/* define modal */
+var modal = document.getElementById("modalid2");
+
+/* define button */
+var btn = document.getElementById("btnid2");
+
+/* define <span> element that closes modal */
+var span = document.getElementsByClassName("close2")[0];
+
+/* function button opens modal */
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+/* on click of <span> (x) modal closes */
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+/* modal closes if clicked anywhere outside of modal */
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+/* modal 3 */
+
+/* define modal */
+var modal = document.getElementById("modalid3");
+
+/* define button */
+var btn = document.getElementById("btnid3");
+
+/* define <span> element that closes modal */
+var span = document.getElementsByClassName("close3")[0];
+
+/* function button opens modal */
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+/* on click of <span> (x) modal closes */
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+/* modal closes if clicked anywhere outside of modal */
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+/* modal 4 */
+
+/* define modal */
+var modal = document.getElementById("modalid4");
+
+/* define button */
+var btn = document.getElementById("btnid4");
+
+/* define <span> element that closes modal */
+var span = document.getElementsByClassName("close4")[0];
+
+/* function button opens modal */
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+/* on click of <span> (x) modal closes */
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+/* modal closes if clicked anywhere outside of modal */
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+/* modal 5 */
+
+/* define modal */
+var modal = document.getElementById("modalid5");
+
+/* define button */
+var btn = document.getElementById("btnid5");
+
+/* define <span> element that closes modal */
+var span = document.getElementsByClassName("close5")[0];
+
+/* function button opens modal */
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+/* on click of <span> (x) modal closes */
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+/* modal closes if clicked anywhere outside of modal */
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+/* modal 6 */
+
+/* define modal */
+var modal = document.getElementById("modalid6");
+
+/* define button */
+var btn = document.getElementById("btnid6");
+
+/* define <span> element that closes modal */
+var span = document.getElementsByClassName("close6")[0];
+
+/* function button opens modal */
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+/* on click of <span> (x) modal closes */
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+/* modal closes if clicked anywhere outside of modal */
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
