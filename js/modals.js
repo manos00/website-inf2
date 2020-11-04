8cf49a8ffb54670733ec9da974351770cef34f6e
manos00 <manos.wagner@gmail.com>
2020-11-04 19:52:57 +0100

    idek

--- js/modals.js
+++ js/modals.js
@@ -1,7 +1,7 @@
 /* modal 1 */
 
 /* define modal */
-var modal = document.getElementById("modalid");
+var modal1 = document.getElementById("modalid");
 
 /* define button */
 var btn = document.getElementById("btnid");
@@ -11,25 +11,25 @@ var span = document.getElementsByClassName("close1")[0];
 
 /* function button opens modal */
 btn.onclick = function () {
-    modal.style.display = "block";
+    modal1.style.display = "block";
 }
 
 /* on click of <span> (x) modal closes */
 span.onclick = function () {
-    modal.style.display = "none";
+    modal1.style.display = "none";
 }
 
 /* modal closes if clicked anywhere outside of modal */
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == modal1) {
+        modal1.style.display = "none";
     }
 }
 
 /* modal 2 */
 
 /* define modal */
-var modal = document.getElementById("modalid2");
+var modal2 = document.getElementById("modalid2");
 
 /* define button */
 var btn = document.getElementById("btnid2");
@@ -39,25 +39,25 @@ var span = document.getElementsByClassName("close2")[0];
 
 /* function button opens modal */
 btn.onclick = function () {
-    modal.style.display = "block";
+    modal2.style.display = "block";
 }
 
 /* on click of <span> (x) modal closes */
 span.onclick = function () {
-    modal.style.display = "none";
+    modal2.style.display = "none";
 }
 
 /* modal closes if clicked anywhere outside of modal */
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == modal2) {
+        modal2.style.display = "none";
     }
 }
 
 /* modal 3 */
 
 /* define modal */
-var modal = document.getElementById("modalid3");
+var modal3 = document.getElementById("modalid3");
 
 /* define button */
 var btn = document.getElementById("btnid3");
@@ -67,17 +67,45 @@ var span = document.getElementsByClassName("close3")[0];
 
 /* function button opens modal */
 btn.onclick = function () {
-    modal.style.display = "block";
+    modal3.style.display = "block";
 }
 
 /* on click of <span> (x) modal closes */
 span.onclick = function () {
-    modal.style.display = "none";
+    modal3.style.display = "none";
 }
 
 /* modal closes if clicked anywhere outside of modal */
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == modal3) {
+        modal3.style.display = "none";
+    }
+}
+
+/* modal 4 */
+
+/* define modal */
+var modal4 = document.getElementById("modalid4");
+
+/* define button */
+var btn = document.getElementById("btnid4");
+
+/* define <span> element that closes modal */
+var span = document.getElementsByClassName("close4")[0];
+
+/* function button opens modal */
+btn.onclick = function () {
+    modal4.style.display = "block";
+}
+
+/* on click of <span> (x) modal closes */
+span.onclick = function () {
+    modal4.style.display = "none";
+}
+
+/* modal closes if clicked anywhere outside of modal */
+window.onclick = function (event) {
+    if (event.target == modal4) {
+        modal4.style.display = "none";
     }
 }
